272ef5fb2825e4588068f8683d1fe59ae8b1e126
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2018-08-12 18:34:26 +0200

    fixed edit component on routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,14 @@ import {LoginFormComponent} from '../components/login-form/login-form.component'
 import {AuthenticationServiceOnLogin} from '../services/authentication-service-on-login.service';
 import {AuthenticationServiceOnHomePage} from '../services/authentication-service-on-home-page.service';
 import {RegistrationFormComponent} from '../components/registration-form/registration-form.component';
+import {EditUserComponent} from '../components/edit-user/edit-user.component';
 
 export const ROUTING: Routes = [
 
   {path: 'login', component: LoginFormComponent, canActivate: [AuthenticationServiceOnLogin]},
   {path: 'registration', component: RegistrationFormComponent},
   {path: 'home', component: HomePageComponent, canActivate: [AuthenticationServiceOnHomePage]},
-  {path: 'edit/:id', component: HomePageComponent},
+  {path: 'edit/:id', component: EditUserComponent},
   {path: '**', redirectTo: '/login'},
 
 ];
