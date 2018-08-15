cf5be1e6aef725888639949e749b56844ad3cac4
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2018-08-15 11:52:47 +0200

    changed layot (added fonts)

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {DataStorageService} from '../../services/data-storage.service';
 import {Router} from '@angular/router';
 
@@ -7,15 +7,21 @@ import {Router} from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   /**
    * @var {boolean} True if user stays on registration page or login page. true on login page.
    */
   @Input() showLinks: boolean = false;
 
+  selectedButton: string;
+
   constructor(private dataStorageService: DataStorageService,
               private router: Router) {
+  }
+
+  ngOnInit() {
+    this.selectedButton = this.router.url;
   }
 
   exit() {
